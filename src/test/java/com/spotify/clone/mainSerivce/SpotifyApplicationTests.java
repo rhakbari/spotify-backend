1e5304271010258c7479e40b90573473514b78a7
rhakbari <rhakbari.96@gmail.com>
2024-02-16 15:18:38 +0500

    first commit

---
+++ src/test/java/com/spotify/clone/mainSerivce/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotify.clone.mainSerivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
